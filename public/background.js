2d9d6eb108fdca159666d2c2bb4eb75bba94caf2
starmucks <xite1@yandex.ru>
2021-01-23 15:09:49 +0300

    possibility to install as google chrome extension

---
+++ public/background.js
@@ -0,0 +1,8 @@
+﻿chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+    chrome.declarativeContent.onPageChanged.addRules([{
+        conditions: [new chrome.declarativeContent.PageStateMatcher({
+            pageUrl: { urlContains: "chrome://newtab" },
+        })],
+        actions: [new chrome.declarativeContent.ShowPageAction()]
+    }]);
+});
